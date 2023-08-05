4ea78fb2cfbf7fc315beca669ed9c4af42a68f45
ruhinahmad <ruhinahmad01@gmail.com>
2023-08-05 15:46:54 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,64 @@
+// var name = 'ruhin'
+// var age = (32)
+// var y = 5
+// console.log(y)
+
+// data types 
+// var age = 4; //number
+// var name = 'mamun' //string
+// var x = true //boolean
+
+// var x = '25.4565';
+// // var y = parseInt(x)
+// var y = parseFloat(x);
+// console.log(typeof y);
+
+// var num = '15.35';
+// var num1 = parseFloat(num)
+// console.log(parseInt (num1));
+
+// var number = '25.43254';
+// var floating = parseFloat(number)
+// console.log(number);
+
+// var number = '25.3535'
+// var floating = parseFloat(number);
+// var fixed = floating.toFixed(2)
+
+// console.log(parseFloat(fixed));
+// var x = 5
+// x = x + 5
+// x = x+1
+// x +=1
+// x++ //post increament
+// ++x // pre increament
+// console.log(x++);
+// // console.log(x++);
+// console.log(++x);
+
+// ARRAY
+//  var arr = ['Programming Hero',15,20]
+//  console.log(arr.length)
+// //  console.log(arr[2])
+//  console.log(arr[2])
+// var fruits = ['jackfruits','litchee']
+// fruits.push('watermelon')
+// fruits.push('banana')
+// fruits.push('mango')
+// fruits.pop[1]
+// var ex = fruits.pop()
+// fruits.pop()
+// console.log(fruits)
+// console.log(ex)
+
+//CONDITIONS
+
+var age = 18;
+
+if(age <= 17){
+    console.log('you are adult')
+}
+else{
+    console.log('you are teenager')
+}
+
